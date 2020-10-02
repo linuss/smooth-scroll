4c18df3d1c79efa7847f2b01e0cce4a5a38e247c
Drew Greene <drew@0ui.org>
2020-10-01 22:40:35 -0700

    clarify some example code

--- example/src/ExampleInner.elm
+++ example/src/ExampleInner.elm
@@ -6,7 +6,7 @@ import Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import SmoothScroll exposing (scrollToWithOptions, defaultConfig, containerElement)
+import SmoothScroll exposing (containerElement, defaultConfig, scrollToWithOptions)
 import Task exposing (Task)
 
 
@@ -49,34 +49,46 @@ update msg model =
 
 view : Model -> Document Msg
 view model =
-    { title = "Foo"
+    { title = "Scroll Inside Elements"
     , body =
-        [ div [ style "position" "fixed", style "top" "0", style "height" "40px", style "left" "0", style "right" "0", style "display" "flex", style "justify-content" "space-around" ]
+        [ div topBarStyles
             [ div []
                 [ button [ onClick (SmoothScroll "left-half" "anchor-left-100") ] [ text "100" ]
-                , button [ onClick (SmoothScroll "left-half" "anchor-left-25")] [ text "25" ]
-                , button [ onClick (SmoothScroll "left-half" "anchor-left-1")] [ text "1" ]
+                , button [ onClick (SmoothScroll "left-half" "anchor-left-25") ] [ text "25" ]
+                , button [ onClick (SmoothScroll "left-half" "anchor-left-1") ] [ text "1" ]
                 ]
             , div []
                 [ button [ onClick (SmoothScroll "right-half" "anchor-right-100") ] [ text "100" ]
-                , button [ onClick (SmoothScroll "right-half" "anchor-right-25")] [ text "25" ]
-                , button [ onClick (SmoothScroll "right-half" "anchor-right-1")] [ text "1" ]
+                , button [ onClick (SmoothScroll "right-half" "anchor-right-25") ] [ text "25" ]
+                , button [ onClick (SmoothScroll "right-half" "anchor-right-1") ] [ text "1" ]
                 ]
             ]
-        , div [ style "font-size" "18px", style "font-family" "sans-serif",  style "position" "fixed", style "top" "40px", style "bottom" "0", style "left" "0", style "right" "0", style "display" "flex" ]
-            [ div [  id "left-half", style "flex-grow" "1", style "overflow-y" "scroll", style "border" "1px solid #ccc", style "background" "cornflowerblue" ]
-                [ ul [  ]
-                    (List.range 1 100
-                        |> List.map (\num -> li [ String.fromInt num |> String.append "anchor-left-" |> id ] [ text <| String.fromInt num ])
-                    )
+        , div rowStyles
+            [ div ([ id "left-half", style "background" "cornflowerblue" ] ++ columnStyles)
+                [ ul []
+                    (List.range 1 100 |> List.map (anchorView "left"))
                 ]
-            , div [  id "right-half", style "flex-grow" "1", style "overflow-y" "scroll", style "border" "1px solid #ccc", style "background" "wheat" ]
-                [ ul [  ]
-                    (List.range 1 100
-                        |> List.map (\num -> li [ String.fromInt num |> String.append "anchor-right-" |> id ] [ text <| String.fromInt num ])
-                    )
+            , div ([ id "right-half", style "background" "wheat" ] ++ columnStyles)
+                [ ul []
+                    (List.range 1 100 |> List.map (anchorView "right"))
                 ]
             ]
         ]
-
     }
+
+
+anchorView side num =
+    li [ String.fromInt num |> String.append ("anchor-" ++ side ++ "-") |> id ]
+        [ text <| String.fromInt num ]
+
+
+topBarStyles =
+    [ style "position" "fixed", style "top" "0", style "height" "40px", style "left" "0", style "right" "0", style "display" "flex", style "justify-content" "space-around" ]
+
+
+rowStyles =
+    [ style "font-size" "18px", style "font-family" "sans-serif", style "position" "fixed", style "top" "40px", style "bottom" "0", style "left" "0", style "right" "0", style "display" "flex" ]
+
+
+columnStyles =
+    [ style "flex-grow" "1", style "overflow-y" "scroll", style "border" "1px solid #ccc" ]
